07226fb41e04273db081f9b522adad9ac8057dd8
[radu radu] <radu.cutui@yahoo.com>
2022-10-16 19:53:35 +0300

    init base page object

--- src/main/java/com/shopping/page/Base.java
+++ src/main/java/com/shopping/page/Base.java
@@ -1,6 +1,24 @@
 package com.shopping.page;
 
-public class Base {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.v85.page.Page;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+public abstract class Base {
+
+    ChromeDriver driver;
+    WebDriverWait waitDriver;
+    private ChromeDriver chromeDriver() {
+        System.setProperty("webdriver.chrome.driver", "./resources/drivers/chromedriver.exe");
+        return new ChromeDriver();
+    }
+    //System.getProperty("user.dir") + "\resources\drivers\chromedriver.exe"
+
+    private void init() {
+        this.driver = chromeDriver();
+        PageFactory.initElements(this.driver, this);
+    }
 
 }
